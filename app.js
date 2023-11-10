892311f8b6bf4217b774031ec04792b3f91e1215
alessandra martera <ale.martera@gmail.com>
2023-11-10 15:17:11 +0100

    res.format

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 3000;
 
+const home = require("./controllers/home.js");
+
+const posts = require("./controllers/posts.js");
+
+app.get("/", home.index);
+/*
 app.get('/', (req, res) => {
- res.send('<h1>Benvenuto nel mio blog!</h1>');
-})
+    res.send('<h1>Benvenuto nel mio blog! da app.js</h1>');
+   })
+*/
 app.listen(port, () => {
  console.log(`Server partito su porta http://localhost:${port}`)
 })
